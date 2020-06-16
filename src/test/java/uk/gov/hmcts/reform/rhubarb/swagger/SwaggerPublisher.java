60ba776eabcf4ebdec4cf4d1bd8588fa75ef9e40
Piyush Kumar <piyush.kumar@hmcts.net>
2020-06-16 15:16:57 +0100

    typos

--- src/test/java/uk/gov/hmcts/reform/rhubarb/swagger/SwaggerPublisher.java
+++ src/test/java/uk/gov/hmcts/reform/rhubarb/swagger/SwaggerPublisher.java
@@ -1,19 +1,19 @@
-package uk.gov.hmcts.reform.rhubarb.recipes;
+package uk.gov.hmcts.reform.rhubarb.swagger;
 
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.ResultActions;
-import static org.springframework.web.context.WebApplicationContext;
-import static org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
-import static org.springframework.boot.test.context.SpringBootTest;
-import static org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import static org.springframework.beans.factory.annotation.Autowired;
-import static org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.BeforeEach;
-import static org.junit.jupiter.api.DisplayName;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
