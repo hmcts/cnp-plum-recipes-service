632e836a1c25400e1efb3260fc274b870692416d
Avinash Mutham <avinashm@Torreira.local>
2020-12-11 10:07:41 +0000

    changing the names of the secrets to v11

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -50,27 +50,39 @@ resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+  name         = "recipe-backend-POSTGRES-DATABASE"
+  value        = module.recipe-database.postgresql_database
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
 resource "azurerm_key_vault_secret" "POSTGRES-USER-V11" {
-  name         = "recipe-backend-POSTGRES-USER"
+  name         = "recipe-backend-POSTGRES-USER-v11"
   value        = module.recipe-database-v11.user_name
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}-v11"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS-V11" {
-  name         = "recipe-backend-POSTGRES-PASS"
+  name         = "recipe-backend-POSTGRES-PASS-v11"
   value        = module.recipe-database-v11.postgresql_password
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}-v11"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_HOST-V11" {
-  name         = "recipe-backend-POSTGRES-HOST"
+  name         = "recipe-backend-POSTGRES-HOST-v11"
   value        = module.recipe-database-v11.host_name
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}-v11"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_PORT-V11" {
-  name         = "recipe-backend-POSTGRES-PORT"
+  name         = "recipe-backend-POSTGRES-PORT-v11"
   value        = module.recipe-database-v11.postgresql_listen_port
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}-v11"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE-V11" {
+  name         = "recipe-backend-POSTGRES-DATABASE-v11"
+  value        = module.recipe-database-v11.postgresql_database
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}-v11"
 }
 
