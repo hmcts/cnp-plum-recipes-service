82d8a139725441f3a90b43421cd295034f842b2a
Avinash Mutham <avinashm@Torreira.lan>
2020-12-14 11:09:03 +0000

    defining the private dns provider

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,12 @@ provider "azurerm" {
   features {}
 }
 
+provider "azurerm" {
+  features {}
+  alias           = "private_dns"
+  subscription_id = var.private_dns_subscription_id
+}
+
 locals {
   app        = "recipe-backend"
   create_api = var.env != "preview" && var.env != "spreview"
