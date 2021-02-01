abcc50300e3385ac9a530ca2a26d93f3546e0ef1
Thomas Thornton <>
2021-02-01 14:57:14 +0000

    no message

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,11 +2,11 @@ provider "azurerm" {
   features {}
 }
 
-# provider "azurerm" {
-#   features {}
-#   alias           = "private_dns"
-#   subscription_id = var.private_dns_subscription_id
-# }
+provider "azurerm" {
+  features {}
+  alias           = "private_dns"
+  subscription_id = var.private_dns_subscription_id
+}
 
 locals {
   app        = "recipe-backend"
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -45,6 +45,6 @@ variable "autoheal" {
   default     = "True"
 }
 
-# variable "private_dns_subscription_id" {
-#   default = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
-# }
+variable "private_dns_subscription_id" {
+  default = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+}
