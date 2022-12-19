f1035d51e591afbea957dd80db0d051f327c4cb1
Daniel Wilson <daniel.wilson@kainos.com>
2022-12-19 14:27:19 +0000

    fix kv definition

--- infrastructure/expAfter-testing.tf
+++ infrastructure/expAfter-testing.tf
@@ -3,7 +3,31 @@ resource "azurerm_resource_group" "expAfter_testing_rg" {
   location = "UK South"
 }
 
-data "azurerm_key_vault" "expAfter_testing_kv" {
+resource "azurerm_key_vault" "expAfter_testing_kv" {
   name                = "expAfter-testing-${var.env}"
+  location            = azurerm_resource_group.expAfter_testing.location
   resource_group_name = azurerm_key_vault.expAfter_testing_rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
 }
