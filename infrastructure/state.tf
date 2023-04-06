41539c9fea5185e8770f82681cd8730b7d1d682b
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2023-04-06 12:44:36 +0100

    Update state.tf

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -5,6 +5,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
+      configuration_aliases = [azurerm.postgres_network]
     }
     random = {
       source = "hashicorp/random"
@@ -12,11 +13,6 @@ terraform {
     azuread = {
       source  = "hashicorp/azuread"
       version = "2.35.0"
-    }
-    azuread = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.7.0"
-      configuration_aliases = [azurerm.postgres_network]
     }
   }
 }
