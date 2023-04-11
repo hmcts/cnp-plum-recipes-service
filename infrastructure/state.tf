fbebfb0f9b534d0af171a0acdfc268703363fee0
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2023-04-11 11:36:50 +0100

    Update state.tf

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -7,7 +7,7 @@ terraform {
       version = "~> 3.0"
       skip_provider_registration = true
       alias                      = "postgres_network"
-      subscription_id            = bf308a5c-0624-4334-8ff8-8dca9fd43783
+      subscription_id            = "bf308a5c-0624-4334-8ff8-8dca9fd43783"
     }
     random = {
       source = "hashicorp/random"
