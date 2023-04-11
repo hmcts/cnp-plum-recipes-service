3ca03491f07aa39ea0336a45390b1864c3893a82
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2023-04-11 11:42:57 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,13 @@ provider "azurerm" {
   features {}
 }
 
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  alias                      = "postgres_network"
+  subscription_id            = var.aks_subscription_id
+}
+
 locals {
   app        = "recipe-backend"
   create_api = var.env != "preview" && var.env != "spreview"
