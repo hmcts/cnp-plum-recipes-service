ca1459456b9af5a39dffaf87486ad98ebfac3cbb
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-04-12 14:13:05 +0100

    Remove var for testing

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -158,23 +158,20 @@ variable "redis_backup_frequency" {
   description = "The Backup Frequency in Minutes. Only supported on Premium SKUs. Possible values are: 15, 30, 60, 360, 720 and 1440"
 }
 
-variable "rdb_storage_account_name" {
-}
-
 module "plum-redis-storage" {
-  source                        = "git@github.com:hmcts/cnp-module-redis?ref=DTSPO-17012-data-persistency"
-  product                       = "${var.product}-${var.component}-session-storage"
-  location                      = var.location
-  env                           = var.env
-  private_endpoint_enabled      = true
-  redis_version                 = "6"
-  business_area                 = "cft"
-  public_network_access_enabled = false
-  common_tags                   = var.common_tags
-  sku_name                      = var.sku_name
-  family                        = var.family
-  capacity                      = var.redis_capacity
-  rdb_backup_enabled            = var.rdb_backup_enabled
-  rdb_backup_frequency          = var.redis_backup_frequency
-  rdb_storage_account_name      = var.product
+  source                          = "git@github.com:hmcts/cnp-module-redis?ref=DTSPO-17012-data-persistency"
+  product                         = "${var.product}-${var.component}-session-storage"
+  location                        = var.location
+  env                             = var.env
+  private_endpoint_enabled        = true
+  redis_version                   = "6"
+  business_area                   = "cft"
+  public_network_access_enabled   = false
+  common_tags                     = var.common_tags
+  sku_name                        = var.sku_name
+  family                          = var.family
+  capacity                        = var.redis_capacity
+  rdb_backup_enabled              = var.rdb_backup_enabled
+  rdb_backup_frequency            = var.redis_backup_frequency
+  rdb_storage_account_name_prefix = var.product
 }
