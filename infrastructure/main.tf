49f838c8c4da156117dc48ae2b4eb8872da20b2c
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-05-28 14:34:09 +0100

    test module branch

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -80,7 +80,7 @@ module "postgresql_flexible" {
     azurerm.postgres_network = azurerm.postgres_network
   }
 
-  source        = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  source        = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=jenkins-permissions"
   env           = var.env
   product       = var.product
   name          = "${var.product}-v14-flexible"
