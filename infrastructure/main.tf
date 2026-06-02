d82bd668362d0b83a5134d0bdbf82b8c77ede656
Nicholas Chang <60232417+NickAzureDevops@users.noreply.github.com>
2026-06-02 09:34:13 +0100

    delete azure cache (#1331)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -110,25 +110,6 @@ module "postgresql_flexible" {
 
 # endregion
 
-module "plum-redis-storage" {
-  source                          = "git@github.com:hmcts/cnp-module-redis?ref=DTSPO-17012-data-persistency-4.x"
-  product                         = "${var.product}-${var.component}-session-storage"
-  location                        = var.location
-  env                             = var.env
-  private_endpoint_enabled        = true
-  redis_version                   = "6"
-  business_area                   = "cft"
-  public_network_access_enabled   = false
-  common_tags                     = var.common_tags
-  sku_name                        = var.sku_name
-  family                          = var.family
-  capacity                        = var.redis_capacity
-  rdb_backup_enabled              = var.rdb_backup_enabled
-  rdb_backup_frequency            = var.redis_backup_frequency
-  rdb_backup_max_snapshot_count   = var.rdb_backup_max_snapshot_count
-  rdb_storage_account_name_prefix = var.product
-}
-
 module "app_service_plan" {
   source = "git@github.com:hmcts/cnp-module-app-service-plan?ref=master"
 
